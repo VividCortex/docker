776062e5886bfb82676c012775dd03a1fe89334b
Mista Marcus Bastian <marcus.bastian@soul-cycle.com>
2018-08-20 17:07:51 -0400

    - Split the build process so that VC_API_TOKEN can be passed in at runtime, rather than at build time. - Migrated entrypoint to a shell script in order to accomplish the above.

---
+++ alpine/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+echo "Checking to make sure VC_API_TOKEN is provided."
+[ "${VC_API_TOKEN}" == "" ] && echo "VC_API_TOKEN not specified." && exit 1
+
+# This part of the install has to happen on the fly because VC_API_TOKEN is required
+# to install. 
+sh install --token=${VC_API_TOKEN} --batch --init=None --static --proxy=dyn
+rm -f install 
+sed '1 a "log-max-size":"5",' /etc/vividcortex/global.conf > /etc/vividcortex/tempconf
+sed '1 a "log-max-backups":"1",' /etc/vividcortex/tempconf > /etc/vividcortex/global.conf
+rm -f /etc/vividcortex/tempconf && chmod 600 /etc/vividcortex/global.conf
+
+# Start the Vividcortex monitoring agent
+echo "Starting the VC agent."
+/usr/local/bin/vc-agent-007 -foreground -forbid-restarts
